7caca193b3a4d530a2373523cb859db10e552b70
H33Kz <qlmejk@gmail.com>
2022-06-19 13:00:45 +0200

    Added main function and first layout for gui + gitignore

---
+++ src/layout.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+import easygui
+import matplotlib
+
+class Menu():
+
+    def __init__(self,root):
+        pass
---
+++ src/main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import layout
+
+def main():
+    root = Tk()
+    root.title('Statistics data analysis tool')
+    
+    
+    m = layout.Menu(root)
+    
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
